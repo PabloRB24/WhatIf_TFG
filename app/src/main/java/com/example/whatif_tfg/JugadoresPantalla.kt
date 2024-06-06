20e8c366cfc4d631b8f422e56827b92eca2f0030
PabloRB24 <pabrod138@alu.centroafuera.es>
2024-06-06 12:26:29 +0200

    JUGADORES

--- app/src/main/java/com/example/whatif_tfg/JugadoresPantalla.kt
+++ app/src/main/java/com/example/whatif_tfg/JugadoresPantalla.kt
@@ -11,6 +11,7 @@ import android.view.ViewTreeObserver
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
@@ -45,7 +46,7 @@ class JugadoresPantalla : AppCompatActivity() {
         val teamName = intent.getStringExtra("equipo")
         if (teamName != null) {
             getPlayersFromTeam(teamName) { playerNames ->
-                recyclerView.adapter = JugadoresAdapter(playerNames)
+                recyclerView.adapter = JugadoresAdapter(playerNames,teamName)
             }
         } else {
             Log.e("JugadoresPantalla", "No se pasó el nombre del equipo en el Intent")
@@ -76,7 +77,7 @@ class JugadoresPantalla : AppCompatActivity() {
 
 
 
-class JugadoresAdapter(private val jugadores: List<String>) : RecyclerView.Adapter<JugadoresAdapter.JugadorViewHolder>() {
+class JugadoresAdapter(private val jugadores: List<String>, private val equipo: String) : RecyclerView.Adapter<JugadoresAdapter.JugadorViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): JugadorViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.item_jugador, parent, false)
@@ -85,7 +86,7 @@ class JugadoresAdapter(private val jugadores: List<String>) : RecyclerView.Adapt
 
     override fun onBindViewHolder(holder: JugadorViewHolder, position: Int) {
         val jugador = jugadores[position]
-        holder.bind(jugador)
+        holder.bind(jugador, equipo)
     }
 
     override fun getItemCount(): Int = jugadores.size
@@ -95,35 +96,97 @@ class JugadoresAdapter(private val jugadores: List<String>) : RecyclerView.Adapt
         private val playerImage: ImageView = itemView.findViewById(R.id.player_image)
         private val placeholderResId = R.drawable.ic_player_placeholder
 
-        fun bind(jugador: String) {
+        fun bind(jugador: String, equipo: String) {
             playerName.text = jugador
-
             val parts = jugador.split(" ")
             if (parts.size == 2) {
-                val firstName = parts[0]
-                val lastName = parts[1]
-                val imageUrl1 = "https://www.2kratings.com/wp-content/uploads/$firstName-$lastName-2K-Rating-547x400.png"
-                val imageUrl2 = "https://www.2kratings.com/wp-content/uploads/$firstName-$lastName-2K-Rating-550x400.png"
+                var firstName = parts[0]
+                var lastName = parts[1]
+                if (firstName.contains("’")) {
+                    firstName = firstName.replace("’", "")
+                }
+                if (lastName.contains("’")) {
+                    lastName = lastName.replace("’", "")
+                }
+                if (firstName.contains(".")) {
+                    firstName = firstName.replace(".", "")
+                }
+                if (lastName.contains(".")) {
+                    lastName = lastName.replace(".", "")
+                }
 
-                loadImageWithFallback(imageUrl1, imageUrl2)
+                val imageUrls = listOf(
+                    "https://www.2kratings.com/wp-content/uploads/$firstName-$lastName-2K-Rating-547x400.png",
+                    "https://www.2kratings.com/wp-content/uploads/$firstName-$lastName-2K-Rating-550x400.png",
+                    "https://www.2kratings.com/wp-content/uploads/$firstName-$lastName-2K-Rating-600x400.png",
+                    "https://www.2kratings.com/wp-content/uploads/$firstName-$lastName-2K-Rating-1-600x400.png",
+                    "https://www.2kratings.com/wp-content/uploads/$firstName-$lastName-2K-Rating-1-547x400.png",
+                    "https://www.2kratings.com/wp-content/uploads/$firstName-$lastName-2K-Rating-1-550x400.png",
+                    "https://www.2kratings.com/wp-content/uploads/$firstName-$lastName-III-2K-Rating-547x400.png",
+                    "https://www.2kratings.com/wp-content/uploads/$firstName-$lastName-II-2K-Rating-547x400.png",
+                    "https://www.2kratings.com/wp-content/uploads/$firstName-$lastName-III-2K-Rating-550x400.png",
+                    "https://www.2kratings.com/wp-content/uploads/$firstName-$lastName-II-2K-Rating-550x400.png",
+                    "https://www.2kratings.com/wp-content/uploads/$firstName-$lastName-III-2K-Rating-600x400.png",
+                    "https://www.2kratings.com/wp-content/uploads/$firstName-$lastName-II-2K-Rating-600x400.png"
+                )
+
+                loadImageWithFallback(imageUrls)
+
             } else if (parts.size >= 3){
-                val firstName = parts[0]
-                val lastName = parts[1]
-                val extra = parts[2]
-                val imageUrl1 = "https://www.2kratings.com/wp-content/uploads/$firstName-$lastName-$extra-2K-Rating-547x400.png"
-                val imageUrl2 = "https://www.2kratings.com/wp-content/uploads/$firstName-$lastName-$extra-2K-Rating-550x400.png"
+                var firstName = parts[0]
+                var lastName = parts[1]
+                var extra = parts[2]
+                if (firstName.contains("’")) {
+                    firstName = firstName.replace("’", "")
+                }
+                if (lastName.contains("’")) {
+                    lastName = lastName.replace("’", "")
+                }
+                if (firstName.contains(".")) {
+                    firstName = firstName.replace(".", "")
+                }
+                if (lastName.contains(".")) {
+                    lastName = lastName.replace(".", "")
+                }
+                if (extra.contains(".")) {
+                    extra = extra.replace(".", "")
+                }
 
-                loadImageWithFallback(imageUrl1, imageUrl2)
+                val imageUrls = listOf(
+                    "https://www.2kratings.com/wp-content/uploads/$firstName-$lastName-$extra-2K-Rating-547x400.png",
+                    "https://www.2kratings.com/wp-content/uploads/$firstName-$lastName-$extra-2K-Rating-550x400.png",
+                    "https://www.2kratings.com/wp-content/uploads/$firstName-$lastName-$extra-2K-Rating-600x400.png",
+                    "https://www.2kratings.com/wp-content/uploads/$firstName-$lastName-$extra-2K-Rating-1-600x400.png"
+                )
+
+                loadImageWithFallback(imageUrls)
+            } else {
+                playerImage.setImageResource(placeholderResId)
+            }
+
+            // Aplicar el fondo correspondiente al equipo
+            val equipoParts = equipo.split(" ")
+            val equipoNombre = equipoParts.last().lowercase()
+            val backgroundResId = itemView.context.resources.getIdentifier("fade_$equipoNombre", "drawable", itemView.context.packageName)
+
+            if (backgroundResId != 0) {
+                itemView.background = ContextCompat.getDrawable(itemView.context, backgroundResId)
             }
         }
 
-        private fun loadImageWithFallback(primaryUrl: String, fallbackUrl: String) {
+        private fun loadImageWithFallback(urls: List<String>, index: Int = 0) {
+            if (index >= urls.size) {
+                // Si todas las URLs fallan, establecer la imagen de error
+                playerImage.setImageResource(placeholderResId)
+                return
+            }
+
             val requestOptions = RequestOptions()
                 .placeholder(placeholderResId)
                 .error(placeholderResId)
 
             Glide.with(itemView.context)
-                .load(primaryUrl)
+                .load(urls[index])
                 .apply(requestOptions)
                 .into(object : CustomTarget<Drawable>() {
                     override fun onResourceReady(resource: Drawable, transition: Transition<in Drawable>?) {
@@ -131,11 +194,8 @@ class JugadoresAdapter(private val jugadores: List<String>) : RecyclerView.Adapt
                     }
 
                     override fun onLoadFailed(errorDrawable: Drawable?) {
-                        // Si la primera URL falla, intentar la segunda
-                        Glide.with(itemView.context)
-                            .load(fallbackUrl)
-                            .apply(requestOptions)
-                            .into(playerImage)
+                        // Si la carga falla, intentar con la siguiente URL
+                        loadImageWithFallback(urls, index + 1)
                     }
 
                     override fun onLoadCleared(placeholder: Drawable?) {
